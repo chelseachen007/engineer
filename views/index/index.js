a29a50a71924e4819f63a476a418216771244c3f
chelseachen007 <aqwejie@qq.com>
2020-09-29 16:54:00 +0800

    test: 测试

--- views/index/index.js
+++ views/index/index.js
@@ -1,4 +1,5 @@
 console.log('启动了')
-function HelloWorld ({ greeting = "hello", greeted = '"World"', silent = false, onMouseOver, }) {
+function HelloWorld ({ greeting = "hello", greeted = '"World"', silent = false, onMouseOver }) {
   let noTrailingComma = [a, b, c]
 };
+         asddddasda
