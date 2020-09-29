cfbff8a3698a14b2643450af5cbdcbabbb55fe05
chelseachen007 <aqwejie@qq.com>
2020-09-29 16:55:01 +0800

    test: bug

--- views/index/index.js
+++ views/index/index.js
@@ -1,5 +1,6 @@
 console.log('启动了')
-function HelloWorld ({ greeting = "hello", greeted = '"World"', silent = false, onMouseOver }) {
-  let noTrailingComma = [a, b, c]
+function HelloWorld ({ 
+  greeting = "hello", greeted = '"World"', silent = false, onMouseOver }) {
+  let noTrailingComma = [a, b, c]；
 };
          asddddasda
