985d12e19c0bb51499720d60a155c7cb65fddc21
chelseachen007 <aqwejie@qq.com>
2020-09-29 19:03:34 +0800

    feat: utils

---
+++ src/utils/utils.js
@@ -0,0 +1,198 @@
+'use strict'
+
+var toString = Object.prototype.toString
+
+/**
+ * 判断是否为 Array
+ *
+ * @param {Object} val The value to test
+ * @returns {boolean} True if the value is Array, otherwise false
+ */
+
+function isArray(val) {
+    return toString.call(val) === '[object Array]'
+}
+
+/**
+ * 判断是否为 undefined
+ *
+ * @param {Object} val The value to test
+ * @returns {boolean} True if the value is undefined, otherwise false
+ */
+function isUndefined(val) {
+    return typeof val === 'undefined'
+}
+
+/**
+ * 判断是否为  is a Buffer
+ *
+ * @param {Object} val The value to test
+ * @returns {boolean} True if value is a Buffer, otherwise false
+ */
+function isBuffer(val) {
+    return (
+        val !== null &&
+        !isUndefined(val) &&
+        val.constructor !== null &&
+        !isUndefined(val.constructor) &&
+        typeof val.constructor.isBuffer === 'function' &&
+        val.constructor.isBuffer(val)
+    )
+}
+
+/**
+ * 判断是否为 ArrayBuffer
+ *
+ * @param {Object} val The value to test
+ * @returns {boolean} True if value is an ArrayBuffer, otherwise false
+ */
+function isArrayBuffer(val) {
+    return toString.call(val) === '[object ArrayBuffer]'
+}
+
+/**
+ * 判断是否为 FormData
+ *
+ * @param {Object} val The value to test
+ * @returns {boolean} True if value is an FormData, otherwise false
+ */
+function isFormData(val) {
+    return typeof FormData !== 'undefined' && val instanceof FormData
+}
+
+/**
+ * 判断是否为 ArrayBuffer
+ *
+ * @param {Object} val The value to test
+ * @returns {boolean} True if value is a view on an ArrayBuffer, otherwise false
+ */
+function isArrayBufferView(val) {
+    var result
+    if (typeof ArrayBuffer !== 'undefined' && ArrayBuffer.isView) {
+        result = ArrayBuffer.isView(val)
+    } else {
+        result = val && val.buffer && val.buffer instanceof ArrayBuffer
+    }
+    return result
+}
+
+/**
+ * 判断是否为 String
+ *
+ * @param {Object} val The value to test
+ * @returns {boolean} True if value is a String, otherwise false
+ */
+function isString(val) {
+    return typeof val === 'string'
+}
+
+/**
+ * 判断是否为 Number
+ *
+ * @param {Object} val The value to test
+ * @returns {boolean} True if value is a Number, otherwise false
+ */
+function isNumber(val) {
+    return typeof val === 'number'
+}
+
+/**
+ * 判断是否为 Object
+ *
+ * @param {Object} val The value to test
+ * @returns {boolean} True if value is an Object, otherwise false
+ */
+function isObject(val) {
+    return val !== null && typeof val === 'object'
+}
+
+/**
+ * 判断是否为 Date
+ *
+ * @param {Object} val The value to test
+ * @returns {boolean} True if value is a Date, otherwise false
+ */
+function isDate(val) {
+    return toString.call(val) === '[object Date]'
+}
+
+/**
+ * 判断是否为 File
+ *
+ * @param {Object} val The value to test
+ * @returns {boolean} True if value is a File, otherwise false
+ */
+function isFile(val) {
+    return toString.call(val) === '[object File]'
+}
+
+/**
+ * 判断是否为Blob
+ *
+ * @param {Object} val The value to test
+ * @returns {boolean} True if value is a Blob, otherwise false
+ */
+function isBlob(val) {
+    return toString.call(val) === '[object Blob]'
+}
+
+/**
+ * 判断是否为Function
+ *
+ * @param {Object} val The value to test
+ * @returns {boolean} True if value is a Function, otherwise false
+ */
+function isFunction(val) {
+    return toString.call(val) === '[object Function]'
+}
+
+/**
+ * 判断是否为 Stream
+ *
+ * @param {Object} val The value to test
+ * @returns {boolean} True if value is a Stream, otherwise false
+ */
+function isStream(val) {
+    return isObject(val) && isFunction(val.pipe)
+}
+
+/**
+ *判断是否为URLSearchParams对象
+ *
+ * @param {Object} val The value to test
+ * @returns {boolean} True if value is a URLSearchParams object, otherwise false
+ */
+function isURLSearchParams(val) {
+    return (
+        typeof URLSearchParams !== 'undefined' && val instanceof URLSearchParams
+    )
+}
+
+/**
+ *删除字符串开头和结尾的多余空格
+ *
+ * @param {String} str The String to trim
+ * @returns {String} The String freed of excess whitespace
+ */
+function trim(str) {
+    return str.replace(/^\s*/, '').replace(/\s*$/, '')
+}
+
+module.exports = {
+    isArray,
+    isArrayBuffer,
+    isBuffer,
+    isFormData,
+    isArrayBufferView,
+    isString,
+    isNumber,
+    isObject,
+    isUndefined,
+    isDate,
+    isFile,
+    isBlob,
+    isFunction,
+    isStream,
+    isURLSearchParams,
+    trim,
+}
