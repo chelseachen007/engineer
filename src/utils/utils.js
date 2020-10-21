05e0c2fc26a8495defcd434965c0236e3d69612e
chelseachen007 <aqwejie@qq.com>
2020-10-21 14:04:57 +0800

    feat: utils

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -178,6 +178,57 @@ function trim(str) {
     return str.replace(/^\s*/, '').replace(/\s*$/, '')
 }
 
+function randomStr(len = 32) {
+    const $chars =
+        'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890'
+    const maxPos = $chars.length
+    let str = ''
+    for (let i = 0; i < len; i++) {
+        str += $chars.charAt(Math.floor(Math.random() * maxPos))
+    }
+    return str
+}
+function typeOf(obj) {
+    const toString = Object.prototype.toString
+    const map = {
+        '[object Boolean]': 'boolean',
+        '[object Number]': 'number',
+        '[object String]': 'string',
+        '[object Function]': 'function',
+        '[object Array]': 'array',
+        '[object Date]': 'date',
+        '[object RegExp]': 'regExp',
+        '[object Undefined]': 'undefined',
+        '[object Null]': 'null',
+        '[object Object]': 'object',
+    }
+    return map[toString.call(obj)]
+}
+// deepCopy
+function deepCopy(data) {
+    const t = typeOf(data)
+    let o
+
+    if (t === 'array') {
+        o = []
+    } else if (t === 'object') {
+        o = {}
+    } else {
+        return data
+    }
+
+    if (t === 'array') {
+        for (let i = 0; i < data.length; i++) {
+            o.push(deepCopy(data[i]))
+        }
+    } else if (t === 'object') {
+        for (let i in data) {
+            o[i] = deepCopy(data[i])
+        }
+    }
+    return o
+}
+
 module.exports = {
     isArray,
     isArrayBuffer,
@@ -195,4 +246,7 @@ module.exports = {
     isStream,
     isURLSearchParams,
     trim,
+    randomStr,
+    typeOf,
+    deepCopy,
 }
